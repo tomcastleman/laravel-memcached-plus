b92b98c0a81c60867656657928ba9348e1d22a32
Tom Castleman <tom@b3it.co>
2016-04-28 17:27:32 +0100

    extracted out validation of custom options. use string|null for persistent connection id

--- src/MemcachedConnector.php
+++ src/MemcachedConnector.php
@@ -10,10 +10,10 @@ class MemcachedConnector
     /**
      * Create a new Memcached connection.
      *
-     * @param array  $servers
-     * @param string $persistentConnectionId
-     * @param array  $customOptions
-     * @param array  $saslCredentials
+     * @param  array  $servers
+     * @param string|null $persistentConnectionId
+     * @param array $customOptions
+     * @param array $saslCredentials
      *
      * @throws \RuntimeException
      *
@@ -21,39 +21,26 @@ class MemcachedConnector
      */
     public function connect(
         array $servers,
-        $persistentConnectionId = false,
+        $persistentConnectionId = null,
         array $customOptions = [],
         array $saslCredentials = []
     ) {
         $memcached = $this->getMemcached($persistentConnectionId);
 
-        // Validate and set custom options
         if (count($customOptions)) {
-            $memcachedConstants = array_map(
-                function ($option) {
-                    $constant = "Memcached::{$option}";
-                    if (!defined($constant)) {
-                        throw new RuntimeException("Invalid Memcached option: [{$constant}]");
-                    }
-
-                    return constant($constant);
-                },
-                array_keys($customOptions)
-            );
-            $memcached->setOptions(array_combine($memcachedConstants, $customOptions));
+            $memcached->setOptions($this->validateCustomOptions($customOptions));
         }
 
-        // Set SASL auth data
+        // Set SASL auth data.
         if (count($saslCredentials) == 2) {
             list($username, $password) = $saslCredentials;
             $memcached->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
             $memcached->setSaslAuthData($username, $password);
         }
 
-        // Only add servers if we need to. If using a persistent connection
-        // the servers must only be added the first time otherwise connections
-        // are duplicated.
-        if (!$memcached->getServerList()) {
+        // Add servers if necessary. When using a persistent connection servers
+        // must only be added once otherwise connections are duplicated.
+        if (! $memcached->getServerList()) {
             foreach ($servers as $server) {
                 $memcached->addServer(
                     $server['host'], $server['port'], $server['weight']
@@ -62,9 +49,11 @@ class MemcachedConnector
         }
 
         $memcachedStatus = $memcached->getVersion();
-        if (!is_array($memcachedStatus)) {
+
+        if (! is_array($memcachedStatus)) {
             throw new RuntimeException('No Memcached servers added.');
         }
+
         if (in_array('255.255.255', $memcachedStatus) && count(array_unique($memcachedStatus)) === 1) {
             throw new RuntimeException('Could not establish Memcached connection.');
         }
@@ -75,14 +64,42 @@ class MemcachedConnector
     /**
      * Get a new Memcached instance.
      *
+     * @param string|null $persistentConnectionId
      * @return \Memcached
      */
     protected function getMemcached($persistentConnectionId)
     {
-        if ($persistentConnectionId !== false) {
+        if (is_string($persistentConnectionId) && strlen($persistentConnectionId)) {
             return new Memcached($persistentConnectionId);
         }
 
         return new Memcached();
+    }
+
+    /**
+     * Validates memcached custom options and resolves them in to
+     * Memcached constants for use by Memcached::setOptions().
+     *
+     * @param array $customOptions
+     *
+     * @throws \RuntimeException
+     *
+     * @return mixed
+     */
+    protected function validateCustomOptions(array $customOptions)
+    {
+        $memcachedConstants = array_map(
+            function ($option) {
+                $constant = "Memcached::{$option}";
+                if (! defined($constant)) {
+                    throw new RuntimeException("Invalid Memcached option: [{$constant}]");
+                }
+
+                return constant($constant);
+            },
+            array_keys($customOptions)
+        );
+
+        return array_combine($memcachedConstants, $customOptions);
     }
 }
